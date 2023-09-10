2a1a995dded5bd89f55a7cc49e9edc5a3ddc7d3a
NaserGharbieh <naserm.gharbieh@gmail.com>
2023-09-10 23:08:53 +0300

    stack-queue-doc

--- linked-list/lib/src/main/java/queue/Queue.java
+++ linked-list/lib/src/main/java/queue/Queue.java
@@ -2,21 +2,21 @@ package queue;
 
 public class Queue<T> {
     private Node<T> front;
+    private Node<T> back;
 
     public Queue() {
         this.front = null;
+        this.back = null;
     }
 
     public void enqueue(T value) {
         Node<T> newNode = new Node<>(value);
         if (isEmpty()) {
             front = newNode;
+            back = newNode;
         } else {
-            Node<T> current = front;
-            while (current.next != null) {
-                current = current.next;
-            }
-            current.next = newNode;
+            back.next = newNode;
+            back = newNode;
         }
     }
 
@@ -26,8 +26,13 @@ public class Queue<T> {
         }
         T value = front.value;
         front = front.next;
+        if (front == null) {
+            back = null;
+        }
         return value;
     }
+
+
 
     public T peek() {
         if (isEmpty()) {
